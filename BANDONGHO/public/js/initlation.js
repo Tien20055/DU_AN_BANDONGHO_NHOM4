ccba34457dc02b73132d4c94cb2abfa5afd7d53f
Tien20055 <nvt02062005@gmail.com>
2025-01-03 09:59:28 +0700

    code

---
+++ BANDONGHO/public/js/initlation.js
@@ -0,0 +1,124 @@
+let products = [{
+    id: 1,
+    status: 1,
+    address: 'Xuất xứ : Thụy Điển',
+    title: 'Đồng hồ nam cao cấp HUB',
+    img: '../imgaes/sp/1.webp',
+    category: 'Nam',
+    price: 200000,
+    desc: 'Đồng hồ HUB cao cấp sang trong phù hợp với các quý ông'
+},
+{
+    id: 2,
+    status: 1,
+    address: 'Xuất xứ : Thụy Điển',
+    title: 'Đồng hồ nam sang trong QSKA',
+    img: '../imgaes/sp/2.webp',
+    category: 'Nam',
+    price: 200000,
+    desc:'Đồng hồ HUB cao cấp sang trong phù hợp với các quý ông'
+},
+{
+    id: 3,
+    status: 1,
+    address: 'Xuất xứ : Thụy Điển',
+    title: 'Đồng hồ hơp thời trang ',
+    img: '../imgaes/sp/3.webp',
+    category: 'Lucsury',
+    price: 200000,
+    desc:'Đồng hồ HUB cao cấp sang trong phù hợp với các quý ông'
+},
+{
+    id: 4,
+    status: 1,
+    address: 'Xuất xứ : Thụy Điển',
+    title: 'Đồng hồ trẻ trung sang trọng',
+    img: '../imgaes/sp/4.webp',
+    category: 'Lucsury',
+    price: 200000,
+    desc:'Đồng hồ HUB cao cấp sang trong phù hợp với các quý ông'
+},
+{
+    id: 5,
+    status: 1,
+    address: 'Xuất xứ : Thụy Điển',
+    title: 'Đồng hồ nữ HUB',
+    img: '../imgaes/sp/7.webp',
+    category: 'Nữ',
+    price: 200000,
+    desc:'Đồng hồ HUB cao cấp sang trong phù hợp với các quý ông'
+},
+{
+    id: 6,
+    status: 1,
+    address: 'Xuất xứ : Thụy Điển',
+    title: 'Đồng hồ nam phù hợp với mọi lứa tuổi',
+    img: '../imgaes/sp/5.webp',
+    category: 'Nam',
+    price: 200000,
+    desc:'Đồng hồ HUB cao cấp sang trong phù hợp với các quý ông'
+},
+{
+    id: 7,
+    status: 1,
+    address: 'Xuất xứ : Thụy Điển',
+    title: 'Đồng hồ nữ sang chảnh',
+    img: '../imgaes/sp/8.webp',
+    category: 'Nữ',
+    price: 200000,
+    desc:'Đồng hồ HUB cao cấp sang trong phù hợp với các quý ông'
+},
+{
+    id: 8,
+    status: 1,
+    address: 'Xuất xứ : Thụy Điển',
+    title: 'Đồng hồ trẻ trung sang trọng',
+    img: '../imgaes/sp/6.webp',
+    category: 'Lucsury',
+    price: 200000,
+    desc:'Đồng hồ HUB cao cấp sang trong phù hợp với các quý ông'
+},
+{
+    id: 9,
+    status: 1,
+    address: 'Xuất xứ : Thụy Điển',
+    title: 'Đồng hồ trẻ trung sang trọng',
+    img: '../imgaes/sp/6.webp',
+    category: 'Lucsury',
+    price: 200000,
+    desc:'Đồng hồ HUB cao cấp sang trong phù hợp với các quý ông'
+},
+{
+    id: 10,
+    status: 1,
+    address: 'Xuất xứ : Thụy Điển',
+    title: 'Đồng hồ trẻ trung sang trọng',
+    img: '../imgaes/sp/6.webp',
+    category: 'Lucsury',
+    price: 200000,
+    desc:'Đồng hồ HUB cao cấp sang trong phù hợp với các quý ông'
+},
+{
+    id: 11,
+    status: 1,
+
+    address: 'Xuất xứ : Thụy Điển',
+    title: 'Đồng hồ trẻ trung sang trọng',
+    img: '../imgaes/sp/6.webp',
+    category: 'Lucsury',
+    price: 200000,
+    desc:'Đồng hồ HUB cao cấp sang trong phù hợp với các quý ông'
+},
+{
+    id: 12,
+    status: 1,
+    
+    address: 'Xuất xứ : Thụy Điển',
+    title: 'Đồng hồ trẻ trung sang trọng',
+    img: '../imgaes/sp/6.webp',
+    category: 'Lucsury',
+    price: 200000,
+    desc:'Đồng hồ HUB cao cấp sang trong phù hợp với các quý ông'
+},
+]
+localStorage.setItem("products" , JSON.stringify(products))
